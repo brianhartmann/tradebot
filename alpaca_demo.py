9b6f225d9b62c55a974c3acbf322fbf68513e5c1
brianhartmann <bjhartmann13@gmail.com>
2024-03-07 11:35:31 -0700

    code cleanup and added comments

--- alpaca_demo.py
+++ alpaca_demo.py
@@ -1,31 +1,14 @@
-# from alpaca.trading.client import TradingClient
-# from alpaca.trading.requests import MarketOrderRequest
-# from alpaca.trading.enums import OrderSide, TimeInForce
-
-# trading_client = TradingClient("PKQ3W1FASCOB7DA4K7V5", "BzX3mYthrDL6Vd4XAjQGO1Pz4uI2TMUxE7hMxsIH")
-
-# market_order_data = MarketOrderRequest(
-#     symbol="AAPL",
-#     qty = 1,
-#     side=OrderSide.BUY,
-#     time_in_force=TimeInForce.DAY
-# )
-
-# market_order = trading_client.submit_order(market_order_data)
-# print(market_order)
-
 from alpaca.data.live import StockDataStream
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-stream = StockDataStream(os.getenv("alpaca_key"), os.getenv("alpaca_secret"))
-stream = StockDataStream("PKQ3W1FASCOB7DA4K7V5", "BzX3mYthrDL6Vd4XAjQGO1Pz4uI2TMUxE7hMxsIH")
+load_dotenv() #loads env file
+stream = StockDataStream(os.getenv("alpaca_key"), os.getenv("alpaca_secret")) #connects to alpaca
 
 async def handle_trade(data):
     print(data.price)
 
-stream.subscribe_trades(handle_trade, "AAPL")
+stream.subscribe_trades(handle_trade, "AAPL") #subscribes to receive AAPL trade updates
 
 stream.run()
 
